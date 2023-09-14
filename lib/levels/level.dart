3142d2d6a0d874425ce56cf91cfb6f7a01f3df9d
siro844 <140879194+siro844@users.noreply.github.com>
2023-09-14 23:36:59 +0530

    added player to the game.

---
+++ lib/levels/level.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+import 'package:pixel_adventure/actors/player.dart';
+
+class Level extends World{
+  final String levelName;
+  Level({required this.levelName});
+late TiledComponent level;
+  @override
+  FutureOr<void> onLoad() async {
+    level=await TiledComponent.load('$levelName.tmx', Vector2.all(16));
+
+    add(level);
+    final spawnPointlayer=level.tileMap.getLayer<ObjectGroup>('SpawnPoints');
+    for(final spawnPoint in spawnPointlayer!.objects){
+        switch (spawnPoint.class_) {
+          case 'Player':
+              final player=Player(character: 'Mask Dude',
+              position: Vector2(spawnPoint.x,spawnPoint.y),
+              );
+              add(player);
+            break;
+          default:
+        }
+    }
+    return super.onLoad();
+  }
+}
---
+++ lib/pixel_adventure.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:pixel_adventure/actors/player.dart';
+import 'package:pixel_adventure/levels/level.dart';
+
+class PixelAdventure extends FlameGame{
+  @override
+  Color backgroundColor()=> const Color(0xFF211F30);
+  final world=Level(
+    levelName: 'level_02',
+  );
+
+  late final CameraComponent cam;
+  @override
+  FutureOr<void> onLoad() async{
+    // load images into cache
+    await images.loadAllImages();
+
+    cam=CameraComponent.withFixedResolution(world: world,width: 640, height: 360);
+  cam.viewfinder.anchor=Anchor.topLeft;
+   addAll([ cam,world]);
+   
+    return super.onLoad();
+  }
+}
